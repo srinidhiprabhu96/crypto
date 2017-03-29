83176f732398b51964cf02b4921f653bb465376b
srinidhiprabhu96 <srinidhiprabhu96@gmail.com>
2017-03-29 18:52:01 +0530

    File ops done

--- impl/file_ops.c
+++ impl/file_ops.c
@@ -1,6 +1,7 @@
 #include<stdio.h>
 #include<stdint.h>
 
+// The space of this union is also 128 bits.
 union bits128
 {
     __uint128_t integer;
@@ -8,54 +9,42 @@ union bits128
 };
 
 // Read part is cupping for some reason
-__uint128_t read_16_bytes(FILE *fp)
+union bits128 read_16_bytes(FILE *fp)
 {
 	__uint128_t ret_val = 0;
 	char buffer;
 	int i;
-	for(i=0;i<16;i++)
-	{
-		fread(&buffer, 1, 1, fp);
-		printf("Buffer value: %d\n",(int)buffer);
-		ret_val = (ret_val*8) + buffer;
-	}
-	union bits128 print;
-	print.integer = ret_val;
-	printf("IN read\n");
+	union bits128 block;
+	//print.integer = ret_val;
 	for(i=15;i>=0;i--)
 	{
-		//fprintf(fpw,"%c",(int)print.byte[i]);
-		printf("%d ",(int)print.byte[i]);
+		fread(&buffer, 1, 1, fp);
+		block.byte[i] = buffer;
+		
 	}
-	printf("\n");
-	return ret_val;
+	return block;
 }
 
-void write_16_bytes(FILE *fpw,__uint128_t value)
+void write_16_bytes(FILE *fpw, union bits128 print)
 {
-	/* // This code has some error.
-	__uint128_t temp;
-	int i,byte;
-	temp = value;
-	for(i=0;i<16;i++)
-	{
-		byte = (int)value%256;
-		printf("%d ",(256+(int)byte)%256);
-		value = value/256;
-	}
-	printf("\n");*/
-	union bits128 print;
-	print.integer = value;
 	int i;
 	// This step depends on the endianness of the system.
 	for(i=15;i>=0;i--)
 	{
 		fprintf(fpw,"%c",(int)print.byte[i]);
-		printf("%d ",(int)print.byte[i]);
 	}
-	printf("\n");
 }
+// This code works fine, but the read and write functions return/expect a union.
+/* union bits128 value,print;
+	__uint128_t abc,def;
+	value = read_16_bytes(fp);
+	abc = value.integer; // Variable abc now has the 128 bit integer.
+	def = function(abc);
+	print.integer = def;
+	write_16_bytes(print); 
+	For example, in the below code I just add 1 to the 128 bit integer and then write to file. I'm able to see that the values printed in the 2 files differ in the LSB of every 16 bit chunk.(Expected result/ 
 
+*/
 // For testing
 int main()
 {
@@ -63,10 +52,12 @@ int main()
 	fp = fopen("../File Generator/5.txt","rb");
 	fpw = fopen("../File Generator/5_write.txt","wb");
 	char buffer;
-	__uint128_t value;
+	union bits128 value;
 	value = read_16_bytes(fp);
+	value.integer = value.integer + 1;
 	write_16_bytes(fpw,value);
 	value = read_16_bytes(fp);
+	value.integer = value.integer + 1;
 	write_16_bytes(fpw,value);
 	return 0;
 }
