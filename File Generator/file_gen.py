5b5c44fc300515c6882a4b5a9c703ef497f63007
srinidhiprabhu96 <srinidhiprabhu96@gmail.com>
2017-03-29 17:22:40 +0530

    Code files for file ops

--- File Generator/file_gen.py
+++ File Generator/file_gen.py
@@ -2,8 +2,20 @@ import random
 
 #print random.randrange(256)
 
+
+#One file generator
+i = 5
+size = pow(2,i)
+filename = str(i)+".txt"
+fp = open(filename,"wb")
+for i in range(0,size):
+	n = random.randrange(256)
+	print n
+	fp.write(chr(n))
+fp.close()
+
+"""
 for i in range(5,21):
-	print i
 	size = pow(2,i)
 	filename = str(i)+".txt"
 	fp = open(filename,"wb")
@@ -11,3 +23,4 @@ for i in range(5,21):
 		n = random.randrange(256)
 		fp.write(chr(n))
 	fp.close()
+"""
