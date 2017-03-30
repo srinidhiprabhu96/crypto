dadaaf720fb7b2409fd2adb75058bdd218589d9c
srinidhiprabhu96 <srinidhiprabhu96@gmail.com>
2017-03-30 18:49:17 +0530

    Counter mode works

--- Implementation/decrypt.c
+++ Implementation/decrypt.c
@@ -9,7 +9,7 @@ void decrypt_file(char *ct_file, char *dt_file, __uint128_t key)
 {
 	FILE *fpr, *fpw;
 	__uint128_t counter = 0xabcdef0987654321, block;
-	union bits128 dt,ct;
+	union bits128 dt,ct,block1,counter1;
 	fpr = fopen(ct_file,"r");
 	fpw = fopen(dt_file,"w");
 	int size,i,n;
@@ -26,9 +26,9 @@ void decrypt_file(char *ct_file, char *dt_file, __uint128_t key)
 	for(i=0;i<n;i++)
 	{
 		ct = read_16_bytes(fpr);
-		block = decrypt(ct.integer, key);		
-		//dt.integer = ct.integer ^ block;
-		dt.integer = block;
+		block = encrypt(counter, key);
+		dt.integer = ct.integer ^ block;
+		//ct.integer = block;
 		write_16_bytes(fpw, dt);
 		counter++;
 	}
@@ -40,7 +40,7 @@ int main(int argc, char *argv[])
 {
 	if(argc != 4)
 	{
-		printf("Usage: ./encrypt <PT_file> <CT_file> <Key file>\n");
+		printf("Usage: ./decrypt.out <PT_file> <CT_file> <Key file>\n");
 		exit(0);
 	}
 	union bits128 key;
--- Implementation/readme.txt
+++ Implementation/readme.txt
@@ -3,10 +3,10 @@
 2) To generate the plaintext files of different sizes
 	python file_gen.py
 	These files will be saved in the "plaintext" folder.
-3) To encrypt a file:
+3) To encrypt a file(using counter mode of operation):
       ./encrypt <path_to_plaintext_file> <path_to_output_file> <key_file>
       These files will be saved in the "ciphertext" folder.
-4) To decrypt a file:
+4) To decrypt a file(using counter mode of operation):
       ./decrypt <path_to_ciphertext_file> <output_file> <key_file>
       These files will be saved in the "decryptedtext" folder.
 5) To plot time vs file size graphs:
